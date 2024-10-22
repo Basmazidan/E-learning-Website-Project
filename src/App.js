b93c450956031d36b348f5224b35564d4bf2db49
amira-mohamed99 <amira8813417@gmail.com>
2024-10-22 16:12:41 +0300

    dash

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import "remixicon/fonts/remixicon.css";
 import SignUp from "./pages/signup/Signup";
-import Dashboard from "./pages/dashboard/Dashboard";
+import Dashboard from './pages/dashboard/Dashboard';
+import Profile from './pages/profile/Profile';
+
 import AuthProvider from "./statestore/Authcontext";
 import CoursesPage from "./pages/coursespage/CoursesPage";
 import CourseDetailPage from "./pages/coursedetailpage/CourseDetailPage";
@@ -25,14 +27,15 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/courses" element={<CoursesPage />} />
             <Route path="/courses/:id" element={<CourseDetailPage />} />
-            <Route path="/Quizs" element={<Quizpage />}/>
-            <Route path="/Quiztype/react" element={<Quiztype quizType="React"/>}/>
-            <Route path="/Quiztype/angular" element={<Quiztype quizType="Angular"/>}/>
-            <Route path="/Quiztype/dotnet" element={<Quiztype quizType="Net"/>}/>
+            <Route path="/Quizs" element={<Quizpage />} />
+            <Route path="/Quiztype/react" element={<Quiztype quizType="React" />} />
+            <Route path="/Quiztype/angular" element={<Quiztype quizType="Angular" />} />
+            <Route path="/Quiztype/dotnet" element={<Quiztype quizType="Net" />} />
           </Routes>
         </Router>
       </AuthProvider>
